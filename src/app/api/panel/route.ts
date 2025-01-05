831ba34c9fe74f838e1161fb060cb7b46c9f4716
aidansunbury <aidansunbury@gmail.com>
2025-01-04 21:39:24 -0800

    update to latest nextjs version

--- src/app/api/panel/route.ts
+++ src/app/api/panel/route.ts
@@ -3,25 +3,9 @@ import { renderTrpcPanel } from "trpc-ui";
 import { appRouter } from "@/server/api/root";
 
 export async function GET(req: Request) {
-	// return new NextResponse(
-	// 	renderTrpcPanel(appRouter, {
-	// 		url: "/api/trpc",
-	// 		transformer: "superjson",
-	// 		meta: {
-	// 			title: "Smart Split aaa",
-	// 		},
-	// 	}),
-	// 	{
-	// 		status: 200,
-	// 		headers: [["Content-Type", "text/html"] as [string, string]],
-	// 	},
-	// );
 	const html = renderTrpcPanel(appRouter, {
 		url: "http://localhost:3000/api/trpc",
 		transformer: "superjson",
-		meta: {
-			title: "hi there lol",
-		},
 	});
 
 	return new NextResponse(html, {
