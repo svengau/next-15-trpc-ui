155a61c8005219f50b56b3c3e0b69817dae2a85c
aidansunbury <aidansunbury@gmail.com>
2025-01-04 21:36:32 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Create T3 App
+
+Project scaffolded to debug [trpc-ui issue](https://github.com/aidansunbury/trpc-ui/issues/53)
+
+
+## setup
+
+```bash
+bun install
+bun dev
+```
+
+No need to setup a database just to see if the panel renders at `http://localhost:3000/api/panel`
---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["next-15-trpc-ui_*"],
+} satisfies Config;
